0f970cae80943fd0d26d74777e67cc58360f2bda
Andy Sh <shinkarev89@gmail.com>
2026-03-13 20:03:55 +0300

    test: add tests for TaskController

---
+++ src/test/java/ru/netology/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Проверяем, что контекст Spring Boot загружается без ошибок
+    }
+}
